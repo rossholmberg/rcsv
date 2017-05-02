758d83744115b2334e14481d978a0b85f995fa1f
Ross Holmberg <ross@holmberg.com.au>
2017-05-02 13:32:09 +1000

    lots of changes, works well, but no list column compatibility

--- R/read_rcsv.R
+++ R/read_rcsv.R
@@ -1,47 +1,76 @@
 read_rcsv <- function( file ) {
 
     head.line <- readLines( con = file, n = 1 )
-    head.line <- sub( "^#head:", "", head.line )
-    column.classes <- strsplit( head.line, split = "," )
-    column.classes <- unlist( column.classes )
+    head.lines <- as.integer( gsub( ".*headlines:|}.*", "", head.line ) )
+    body.rows <- as.integer( gsub( ".*bodyrows:|}.*", "", head.line ) )
+
+    header <- readLines( con = file, n = head.lines )[ -1L ]
+
+    column.names <- gsub( ".*colname:|}.*", "", header )
+
+    column.classes <- gsub( ".*colclass:|}.*", "", header )
+
+    column.classes.readin <- column.classes
+    column.classes.tofollowup <- which( column.classes.readin %chin% c( "POSIXct", "Date", "factor", "times" ) )
+    column.classes.readin[ column.classes.tofollowup ] <- "character"
 
     output <- fread( file = file,
-                     skip = 1L,
+                     skip = head.lines,
+                     col.names = column.names,
                      sep = ",", sep2 = c( "", "|", "" ),
-                     colClasses = rep( "character", times = length( column.classes ) )
+                     colClasses = column.classes.readin
     )
 
     # adjust all non-list columns to the classes they should be
-    for( col in which( !grepl( "list", column.classes ) ) ) {
-        output[ , ( col ) := switch( EXPR = column.classes[ col ],
-                                     "Date" = as.Date( .SD[[col]], format = "%Y-%m-%d" ),
-                                     "POSIXct" = as.POSIXct( .SD[[col]], format = "%Y-%m-%d %H:%M:%S" ),
-                                     "times" = chron::times( .SD[[col]] ),
-                                     as( .SD[[col]], column.classes[ col ] )
-        ) ]
+    for( col in column.classes.tofollowup ) {
+        col.class <- column.classes[ col ]
+        if( col.class == "Date" ) {
+            output[ , ( col ) := as.Date( .SD[[col]], format = "%Y-%m-%d" ) ]
+
+        } else if( col.class == "POSIXct" ) {
+            tz <- gsub( ".*tz:|}.*", "", header[ col ] )
+            if( tz == "none" ) {
+                warning( paste0( "Timezone is not set for POSIXct column: `",
+                                 column.names[ col ],
+                                 "`. Beware of possible consequences." )
+                )
+                output[ , ( col ) := as.POSIXct( .SD[[col]], format = "%Y-%m-%d %H:%M:%S" ) ]
+            } else {
+                output[ , ( col ) := as.POSIXct( .SD[[col]], format = "%Y-%m-%d %H:%M:%S", tz = tz ) ]
+            }
+
+        } else if( col.class == "times" ) {
+            output[ , ( col ) := chron::times( .SD[[col]] ) ]
+
+        } else if( col.class == "factor" ) {
+            factor.levels <- gsub( ".*levels:|}.*", "", header[ col ] )
+            factor.levels <- unlist( strsplit( factor.levels, "," ) )
+            output[ , ( col ) := factor( .SD[[col]], levels = factor.levels ) ]
+        }
+
     }
 
     # adjust list columns to make list elements the classes they should be
-    for( col in which( grepl( "^list", column.classes ) ) ) {
-        # split the list elements
-        output[ , ( col ) := sapply( .SD[[col]], strsplit, split = "\\|" ) ]
-        # read the class for list elements from the head line
-        list.class <- gsub( "^list\\(|\\)$", "", column.classes[ col ] )
-
-        output[ , ( col ) := switch( EXPR = list.class,
-                                     "Date" = sapply( .SD[[col]], as.Date, format = "%Y-%m-%d" ),
-                                     "POSIXct" = sapply( .SD[[col]], as.POSIXct, format = "%Y-%m-%d %H:%M:%S" ),
-                                     "times" = sapply( .SD[[col]], function(x) {
-                                         if( is.na( suppressWarnings( as.numeric( x[1] ) ) ) ) {
-                                             chron::chron( times. = x, format = "h:m:s", out.format = "h:m:s" )
-                                         } else {
-                                             chron::chron( times. = x, out.format = "h:m:s" )
-                                         }
-                                     } ),
-                                     sapply( .SD[[col]], function(x) {
-                                         as( x, list.class )
-                                     } ) ) ]
-    }
+    # for( col in which( grepl( "^list", column.classes ) ) ) {
+    #     # split the list elements
+    #     output[ , ( col ) := sapply( .SD[[col]], strsplit, split = "\\|" ) ]
+    #     # read the class for list elements from the head line
+    #     list.class <- gsub( "^list\\(|\\)$", "", column.classes[ col ] )
+    #
+    #     output[ , ( col ) := switch( EXPR = list.class,
+    #                                  "Date" = sapply( .SD[[col]], as.Date, format = "%Y-%m-%d" ),
+    #                                  "POSIXct" = sapply( .SD[[col]], as.POSIXct, format = "%Y-%m-%d %H:%M:%S" ),
+    #                                  "times" = sapply( .SD[[col]], function(x) {
+    #                                      if( is.na( suppressWarnings( as.numeric( x[1] ) ) ) ) {
+    #                                          chron::chron( times. = x, format = "h:m:s", out.format = "h:m:s" )
+    #                                      } else {
+    #                                          chron::chron( times. = x, out.format = "h:m:s" )
+    #                                      }
+    #                                  } ),
+    #                                  sapply( .SD[[col]], function(x) {
+    #                                      as( x, list.class )
+    #                                  } ) ) ]
+    # }
 
     return( output )
 
--- R/write_rcsv.R
+++ R/write_rcsv.R
@@ -1,80 +1,112 @@
-write_rcsv <- function( table, file ) {
+write_rcsv <- function( table,
+                        file ) {
+
+
+    # make sure the input object qualifies as a data frame
+    if( !"data.frame" %chin% class( table ) ) {
+        convert.DT.attempt <- try( data.table::setDT( table ), silent = TRUE )
+        if( class( convert.DT.attempt ) == "try-error" ) {
+        stop( "Object for writing could not be converted to a data frame." )
+        } else {
+            warning( "Object has been coerced to data frame for writing as rcsv." )
+        }
+    } else {
+        data.table::setDT( table )
+    }
 
     # make a copy of the input object. This is inefficient in terms of memory,
     # but will prevent data.table making in-place changes to the object.
     input <- data.table::copy( table )
 
-    data.table::setDT( input )
+    # define how many lines are to be used in the header, and how many in the body
+    head.lines <- ncol( input ) + 1L
+    body.rows <- nrow( input )
 
-    # make sure the input object qualifies as a data frame
-    if( !"data.frame" %chin% class( input ) ) {
-        setDT( input )
-        # convert.DT.attempt <- try( setDT( input ), silent = TRUE )
-        # if( class( convert.DT.attempt ) == "try-error" ) {
-            # stop( "Object for writing could not be converted to a data frame." )
-        # } else {
-            warning( "Object has been coerced to data frame for writing as rcsv." )
-        # }
-    } else {
-        data.table::setDT( input )
-    }
+    # put that into a header line
+    head.line.readlines <- paste(
+        "rcsvHeader",
+        paste0( "headlines:", head.lines ),
+        paste0( "bodyrows:", body.rows ),
+        sep = "},{"
+    )
 
-    # convert to data.table if not already
-    if( !is.data.table( input ) ) setDT( input )
+    # find the column names
+    column.names <- names( input )
+    column.names.header <- paste0( "colname:", column.names )
 
     # find the current class of each column
     column.classes <- lapply( input, class )
     column.classes <- sapply( column.classes, "[", 1L )
+    column.classes.header <- paste0( "colclass:", column.classes )
 
-    # work specifically on any list columns
-    for( col in which( column.classes == "list" ) ) {
+    # create text header to start the output file.
+    # this will be used when reading the file back into R
+    header <- paste( column.names.header, column.classes.header, sep = "},{" )
 
-        # make sure all list elements are vectors, and not lists themselves
-        element.listCheck <- sapply( input[[ col ]],
-                                 function(x) {
-                                     { is.vector( x ) && !is.list( x ) } ||
-                                         class( x ) %chin% c( "Date", "POSIXct", "times" )
-                                 }
+    # add a levels parameter to any factor columns
+    if( "factor" %chin% column.classes ) {
+        factor.cols <- which( column.classes == "factor" )
+        header.factor.cols <- lapply(
+            X = input[ , factor.cols, with = FALSE ],
+            FUN = levels
         )
-        if( any( !element.listCheck ) ) {
-            stop( paste( "Column", names( input )[ col ], "contains either non vector or list elements.",
-                         "\nrcsv does not support this column type." ) )
-        }
+        header.factor.cols <- sapply( header.factor.cols, paste, collapse = "," )
+        header.factor.cols <- paste0( "levels:", header.factor.cols )
+        header[ factor.cols ] <- paste( header[ factor.cols ],
+                                        header.factor.cols,
+                                        "from:string",
+                                        sep = "},{" )
+    }
 
-        # find the class/classes of list elements
-        list.class <- sapply( input[[ col ]], class )
-        list.class <- unique( list.class[1] )
+    # add timezone to any POSIXct columns
+    if( "POSIXct" %chin% column.classes ) {
+        posix.cols <- which( column.classes == "POSIXct" )
+        header.posix.cols <- vapply(
+            X = input[ , posix.cols, with = FALSE ],
+            FUN = function(x) {
+                tz <- attr( x, "tzone" )
+                if( !is.null( tz ) ) {
+                    return( tz )
+                } else {
+                    return( "none" )
+                }
+            },
+            FUN.VALUE = vector( "character", length = length( posix.cols ) )
+        )
+        header.posix.cols <- paste0( "tz:", header.posix.cols )
+        header[ posix.cols ] <- paste( header[ posix.cols ],
+                                       header.posix.cols,
+                                       "from:string",
+                                       sep = "},{" )
+    }
 
-        # special case for times class. Make sure no values > 1
-        # (ie: later than midnight, but no date value is specified.)
-        if( list.class == "times" ) {
-            if( max( as.numeric( unlist( input[[ col ]] ) ) ) > 1 ) {
-                stop( paste( "Column", names( input )[ col ], "of class `times` contains values
-                                greater than 1 (ie: later than midnight, with no date value specified.",
-                             "\nThese values won't be saved and read properly.",
-                             "\nConsider coercing this column to a different class." ) )
-            }
-        }
-
-        # make sure the list has a consistent class, otherwise coerce to character class and warn
-        if( length( list.class ) == 1L ) {
-            column.classes[ col ] <- paste0( column.classes[ col ], "(", list.class, ")" )
-            input[ , ( col ) := lapply( .SD[[col]], as, Class = "character" ) ]
-        } else {
-            warning( paste( "List column", names( input )[ col ], "is not of a consistent class.",
-                            "\nWill be written as character." ) )
-            column.classes[ col ] <- paste0( column.classes[ col ], "(character)" )
-            input[ , ( col ) := lapply( .SD[[col]], as, Class = "character" ) ]
+    # convert any columns of `timeas` class to character before writing
+    if( "times" %chin% column.classes ) {
+        times.cols <- which( column.classes == "times" )
+        for( col in times.cols ) {
+            input[ , ( col ) := as( .SD[[col]], "character" ) ]
+            header[ col ] <- paste( header[ col ],
+                                    "from:string",
+                                    sep = "},{" )
         }
     }
 
-    # create a line of text to start the output file.
-    # this will be used when reading the file back into R
-    head.line <- paste( column.classes, collapse = "," )
-    head.line <- paste0( "#head:", head.line, "\n" )
+    if( "Date" %chin% column.classes ) {
+        date.cols <- which( column.classes == "Date" )
+        for( col in date.cols ) {
+            input[ , ( col ) := as( .SD[[col]], "character" ) ]
+            header[ col ] <- paste( header[ col ],
+                                    "from:string",
+                                    sep = "},{" )
+        }
+    }
 
-    # write the head line to the output file
-    cat( head.line, file = file, append = FALSE )
+    # write the header to the output file
+    header <- paste0( "#{", c( head.line.readlines, header ), "}" )
+    cat( header,
+         sep = "\n",
+         file = file, append = FALSE
+    )
 
     # now write the data out, appending below the head line
     fwrite( input,
